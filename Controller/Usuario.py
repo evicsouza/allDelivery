096aa4373a7fa4418c44d87ecfb62d3e572bb8d6
JOSE WANDERLEY PEREIRA DA SILVA <20192ADSGR0184@LABS.GARANHUNS.IFPE>
2019-10-22 19:47:30 -0300

    Classe Usuário

--- Controller/Usuario.py
+++
@@ -1,9 +0,0 @@
-class Usuario:
-    def __init__():
-        self.nome = nome
-        self.email = email
-        self.senha = senha
-        self.cpf = cpf
-    def setNome(self, nome):
-        self.nome = nome
-    
