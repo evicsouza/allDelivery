554a6950210824d4140ec68662e4df2dcd29f872
JOSE WANDERLEY PEREIRA DA SILVA <20192ADSGR0184@LABS.GARANHUNS.IFPE>
2019-10-22 19:54:43 -0300

    add get e set

--- Model/Usuario.py
+++ Model/Usuario.py
@@ -9,7 +9,7 @@ class Usuario:
     def getNome(self):
         return self.nome
     def setEmail(self,email):
-        self.nome = nome
+        self.email = email
     def getEmail(self):
         return self.email
     def setSenha(self,senha):
